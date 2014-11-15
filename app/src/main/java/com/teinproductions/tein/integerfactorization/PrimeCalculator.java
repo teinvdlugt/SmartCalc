8cd0fffdc0124a18daa690592094abf8ce05ec5b
Tein van der Lugt <teinvdlugt@gmail.com>
2014-11-15 16:31:32 +0100

    Make findGCF and findLCM in PrimeCalculator

--- app/src/main/java/com/teinproductions/tein/integerfactorization/PrimeCalculator.java
+++ app/src/main/java/com/teinproductions/tein/integerfactorization/PrimeCalculator.java
@@ -60,4 +60,125 @@ public class PrimeCalculator {
         return result;
     }
 
+    public static ArrayList<Integer> convertToArrayList(Integer[] integers) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        for (Integer integer : integers) {
+            arrayList.add(integer);
+        }
+
+        return arrayList;
+    }
+
+    public static Integer findGCF(Integer int1, Integer int2) {
+        Integer[] factors1 = factorize(int1);
+        Integer[] factors2 = factorize(int2);
+
+        ArrayList<Integer> factorList1 = convertToArrayList(factors1);
+        ArrayList<Integer> factorList2 = convertToArrayList(factors2);
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        for (int num : factorList1) {
+            if (factorList2.contains(num)) {
+                numbers.add(num);
+                factorList2.remove(num);
+            }
+        }
+
+        Integer GCF = 1;
+
+        for (int number : numbers) {
+            GCF *= number;
+        }
+
+        return GCF;
+
+    }
+
+    public static Integer findGCF(Integer[] factors1, Integer[] factors2) {
+        ArrayList<Integer> factorList1 = new ArrayList<Integer>();
+        ArrayList<Integer> factorList2 = new ArrayList<Integer>();
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        for (Integer factor : factors1) {
+            factorList1.add(factor);
+        }
+        for (Integer factor : factors2) {
+            factorList2.add(factor);
+        }
+
+        for (int num : factorList1) {
+            if (factorList2.contains(num)) {
+                numbers.add(num);
+                factorList2.remove(num);
+            }
+        }
+
+        Integer GCF = 1;
+
+        for (int number : numbers) {
+            GCF *= number;
+        }
+
+        return GCF;
+    }
+
+    public static Integer findLCM(Integer int1, Integer int2) {
+        Integer[] factors1 = factorize(int1);
+        Integer[] factors2 = factorize(int2);
+
+        ArrayList<Integer> factorList1 = convertToArrayList(factors1);
+        ArrayList<Integer> factorList2 = convertToArrayList(factors2);
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        for (Integer num : factorList1) {
+            numbers.add(num);
+            if (factorList2.contains(num)) {
+                factorList2.remove(num);
+            }
+        }
+
+        for (Integer num : factorList2) {
+            numbers.add(num);
+        }
+
+        Integer LCM = 1;
+
+        for(Integer num : numbers) {
+            LCM *= num;
+        }
+
+        return LCM;
+
+    }
+
+    public static Integer findLCM(Integer[] factors1, Integer[] factors2) {
+        ArrayList<Integer> factorList1 = convertToArrayList(factors1);
+        ArrayList<Integer> factorList2 = convertToArrayList(factors2);
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        for (Integer num : factorList1) {
+            numbers.add(num);
+            if (factorList2.contains(num)) {
+                factorList2.remove(num);
+            }
+        }
+
+        for (Integer num : factorList2) {
+            numbers.add(num);
+        }
+
+        Integer LCM = 1;
+
+        for(Integer num : numbers) {
+            LCM *= num;
+        }
+
+        return LCM;
+
+    }
+
 }
