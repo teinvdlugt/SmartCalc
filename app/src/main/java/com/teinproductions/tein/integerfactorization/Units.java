dcd2387932901ff148e883587a893ae431faeba4
Tein van der Lugt <teinvdlugt@gmail.com>
2014-11-22 14:54:55 +0100

    Make Unit for Time

--- app/src/main/java/com/teinproductions/tein/integerfactorization/Units.java
+++ app/src/main/java/com/teinproductions/tein/integerfactorization/Units.java
@@ -58,4 +58,64 @@ public class Units {
 
     }
 
+    public enum Time {
+        NANOSEC(1000000000.0, R.string.nanoseconds, R.string.nanoseconds_abbr),
+        MICROSEC(1000000.0, R.string.microsecond, R.string.microsecond_abbr),
+        MILLISEC(1000.0, R.string.millisecond, R.string.millisecond_abbr),
+        SEC(1.0, R.string.second, R.string.second_abbr),
+        MIN(1.0 / 60.0, R.string.minute, R.string.minute_abbr),
+        HOUR(1.0 / 3600.0, R.string.hour, R.string.hour_abbr),
+        DAY(1.0 / 86400.0, R.string.day, R.string.day_abbr),
+        WEEK(1.0 / 604800.0, R.string.week, R.string.week_abbr),
+        MONTH(1.0 / 2630000.0, R.string.month, R.string.month_abbr),
+        YEAR(1.0 / 31560000.0, R.string.years, R.string.years_abbr),
+        DECADE(1.0 / 315600000.0, R.string.decade, R.string.decade_abbr),
+        CENTURY(1.0 / 3156000000.0, R.string.century, R.string.century_abbr);
+
+        Time(Double factor, int word, int abbreviation) {
+            this.factor = factor;
+            this.word = word;
+            this.abbreviation = abbreviation;
+        }
+
+        private Double factor;
+        private int word, abbreviation;
+
+        public static Double convert(Time original, Time converted, Double value) {
+            return value / original.getFactor() * converted.getFactor();
+        }
+
+        public static String[] getWords(Context context) {
+            String[] output = new String[Time.values().length];
+
+            for (int i = 0; i < Time.values().length; i++) {
+                output[i] = Time.values()[i].getWord(context);
+            }
+
+            return output;
+        }
+
+        public static String[] getAbbreviations(Context context) {
+            String[] output = new String[Time.values().length];
+
+            for (int i = 0; i < Time.values().length; i++) {
+                output[i] = Time.values()[i].getAbbreviation(context);
+            }
+
+            return output;
+        }
+
+        public Double getFactor() {
+            return this.factor;
+        }
+
+        public String getWord(Context context) {
+            return context.getResources().getString(this.word);
+        }
+
+        public String getAbbreviation(Context context) {
+            return context.getResources().getString(this.abbreviation);
+        }
+    }
+
 }
