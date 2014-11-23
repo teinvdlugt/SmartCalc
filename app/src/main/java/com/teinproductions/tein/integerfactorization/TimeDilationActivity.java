64105900fc878be0428234edd074525088ae8759
Tein van der Lugt <teinvdlugt@gmail.com>
2014-11-23 17:46:28 +0100

    When changing unit, recalculate (TimeDilation)

--- app/src/main/java/com/teinproductions/tein/integerfactorization/TimeDilationActivity.java
+++ app/src/main/java/com/teinproductions/tein/integerfactorization/TimeDilationActivity.java
@@ -2,6 +2,7 @@ package com.teinproductions.tein.integerfactorization;
 
 import android.text.InputType;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
 public class TimeDilationActivity extends EditTextActivity {
@@ -39,6 +40,22 @@ public class TimeDilationActivity extends EditTextActivity {
         spinner2.setSelection(3);
         resultSpinner.setSelection(3);
 
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                recalculate();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+
+        spinner1.setOnItemSelectedListener(itemSelectedListener);
+        spinner2.setOnItemSelectedListener(itemSelectedListener);
+        resultSpinner.setOnItemSelectedListener(itemSelectedListener);
+
         clickButtonWhenFilledEditText(editText2);
     }
 
@@ -67,6 +84,32 @@ public class TimeDilationActivity extends EditTextActivity {
             resultTextView.setText(Units.format(result));
         } catch (NumberFormatException e) {
             CustomDialog.invalidNumber(getFragmentManager());
+        }
+    }
+
+    protected void recalculate() {
+        try {
+            Double input1 = Double.parseDouble(editText1.getText().toString());
+            Double input2 = Double.parseDouble(editText2.getText().toString());
+
+            Units.Velocity velocity1 = Units.Velocity.values()[(spinner1.getSelectedItemPosition())];
+            Units.Time time2 = Units.Time.values()[(spinner2.getSelectedItemPosition())];
+            Units.Time resultTime = Units.Time.values()[(resultSpinner.getSelectedItemPosition())];
+
+            Double num1 = velocity1.convertTo(Units.Velocity.MPS, input1);
+            Double num2 = time2.convertTo(Units.Time.SEC, input2);
+
+            if (num1 > Units.Velocity.C) {
+                throw new NumberFormatException(getString(R.string.faster_than_light));
+            }
+
+            Double dilated = num2 / (Math.sqrt(1 - ((num1 * num1) / (Units.Velocity.C * Units.Velocity.C))));
+
+            Double result = Units.Time.SEC.convertTo(resultTime, dilated);
+
+            resultTextView.setText(Units.format(result));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
         }
     }
 }
