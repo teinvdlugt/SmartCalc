61b243a9099a2ef0909f50da1190cc84307631d3
Tein van der Lugt <teinvdlugt@gmail.com>
2014-12-01 19:34:22 +0100

    Make color when touched item in BMIInfo list

--- app/src/main/java/com/teinproductions/tein/integerfactorization/BMIInfoActivity.java
+++ app/src/main/java/com/teinproductions/tein/integerfactorization/BMIInfoActivity.java
@@ -1,17 +1,28 @@
 package com.teinproductions.tein.integerfactorization;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class BMIInfoActivity extends ActionBarActivity {
+
+    LinearLayout rootLayout;
+    Integer backgroundColor;
+    Integer animDuration;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +35,59 @@ public class BMIInfoActivity extends ActionBarActivity {
 
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(new BMIInfoAdapter(this, names, values));
+
+        rootLayout = (LinearLayout) findViewById(R.id.root_layout);
+        animDuration = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                BMIActivity.BMIState state = BMIActivity.BMIState.values()[position];
+                animateBackgroundColor(state);
+            }
+        });
+
     }
+
+    private void animateBackgroundColor(BMIActivity.BMIState state) {
+        final Integer colorFrom;
+        if (backgroundColor == null) {
+            colorFrom = getResources().getColor(android.R.color.white);
+        } else {
+            colorFrom = backgroundColor;
+        }
+        final Integer colorTo = getResources().getColor(state.getColor());
+
+        backgroundColor = colorTo;
+
+        final ValueAnimator animator = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+        animator.setDuration(animDuration);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                rootLayout.setBackgroundColor((Integer) animation.getAnimatedValue());
+            }
+        });
+
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                final ValueAnimator actionBarAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+                actionBarAnimator.setDuration(animDuration);
+                actionBarAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable((Integer) animation.getAnimatedValue()));
+                    }
+                });
+                actionBarAnimator.start();
+            }
+        });
+
+        animator.start();
+
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
