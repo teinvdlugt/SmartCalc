f0b70e85aca59c96b6d0fa5cbdf6d1d5a7ece156
Tein van der Lugt <teinvdlugt@gmail.com>
2014-12-11 21:46:14 +0100

    Use SlidingTabLayout instead of deprecated action bar tabs

--- app/src/main/java/com/teinproductions/tein/integerfactorization/ElementPagerActivity.java
+++ app/src/main/java/com/teinproductions/tein/integerfactorization/ElementPagerActivity.java
@@ -10,95 +10,45 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class ElementPagerActivity extends ActionBarActivity
-                    implements ElementListFragment.onElementClickListener,
-                               CalculateFragment.OnCalculateClickListener{
+        implements ElementListFragment.onElementClickListener,
+        CalculateFragment.OnCalculateClickListener {
 
-    ViewPager theViewPager;
-    private ActionBar actionBar;
+    private ViewPager theViewPager;
+    private SlidingTabLayout slidingTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_element_pager);
 
-        actionBar = getSupportActionBar();
-
-        theViewPager = new ViewPager(this);
-        theViewPager.setId(0x1);
+        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        theViewPager = (ViewPager) findViewById(R.id.view_pager);
 
         theViewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(int position) {
-                if(position == 0){
+                if (position == 0) {
                     return new ElementListFragment();
-                } return Element.values()[position - 1].toFragment();
+                }
+                return Element.values()[position - 1].toFragment();
             }
 
             @Override
             public int getCount() {
                 return Element.values().length + 1;
             }
-        });
-
-        theViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int i, float v, int i2) {
-
-            }
 
             @Override
-            public void onPageSelected(int position) {
-                invalidateOptionsMenu();
-                if(position == 0){
-                    actionBar.setTitle(R.string.elements);
-                    actionBar.selectTab(null);
-                } else {
-                    actionBar.setTitle(Element.values()[position - 1].getName(ElementPagerActivity.this));
-                    actionBar.selectTab(actionBar.getTabAt(position - 1));
+            public CharSequence getPageTitle(int position) {
+                if (position == 0) {
+                    return getString(R.string.action_bar_pager_show_list);
                 }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int i) {
-
+                return Element.values()[position - 1].getName(ElementPagerActivity.this);
             }
         });
-
-
-        actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        ActionBar.TabListener tabListener = new ActionBar.TabListener(){
-
-            @Override
-            public void onTabSelected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
-                int position = tab.getPosition();
-                theViewPager.setCurrentItem(position + 1, true);
-            }
-
-            @Override
-            public void onTabUnselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
-
-            }
-
-            @Override
-            public void onTabReselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
-                int position = tab.getPosition();
-                theViewPager.setCurrentItem(position + 1, true);
-            }
-        };
-
-        for (int i = 0; i < Element.values().length; i++){
-        actionBar.addTab(
-                actionBar.newTab()
-                .setText(Element.values()[i].getName(this))
-                .setTabListener(tabListener));
-        }
 
         theViewPager.setCurrentItem(0);
-        actionBar.selectTab(null);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
-        setContentView(theViewPager);
+        slidingTabLayout.setViewPager(theViewPager);
     }
 
 
@@ -111,7 +61,7 @@ public class ElementPagerActivity extends ActionBarActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.pager_activity_show_list_action:
                 theViewPager.setCurrentItem(0, true);
                 return true;
@@ -124,7 +74,7 @@ public class ElementPagerActivity extends ActionBarActivity
 
     @Override
     public void onBackPressed() {
-        if (theViewPager.getCurrentItem() == 0){
+        if (theViewPager.getCurrentItem() == 0) {
             super.onBackPressed();
         } else {
             theViewPager.setCurrentItem(0);
